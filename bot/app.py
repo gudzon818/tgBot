de41253b5b0ebe274da52d37fa21b773f14a21da
Kirill Akulenko <k.akulenko@instadev.net>
2025-11-11 14:37:49 +0300

    кнопки хелп и пинг

--- bot/app.py
+++ bot/app.py
@@ -7,6 +7,8 @@ from aiogram.enums import ParseMode
 
 from bot.core.settings import settings
 from bot.handlers.start import router as start_router
+from bot.handlers.help import router as help_router
+from bot.handlers.ping import router as ping_router
 from bot.middlewares.anti_flood import AntiFloodMiddleware
 
 
@@ -17,8 +19,10 @@ async def main() -> None:
     # Middlewares
     dp.message.middleware.register(AntiFloodMiddleware(limit_seconds=1.5))
     dp.include_router(start_router)
+    dp.include_router(help_router)
+    dp.include_router(ping_router)
 
-    logging.info("Bot is starting long polling... (Day 3)")
+    logging.info("Bot is starting long polling... (Day 4)")
     await dp.start_polling(bot)
 
 
--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,9 +1,10 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
+from bot.keyboards.main import main_menu
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: types.Message) -> None:
-    await message.answer("Привет! Бот запущен. 🚀")
+    await message.answer("Привет! Бот запущен. 🚀", reply_markup=main_menu())
